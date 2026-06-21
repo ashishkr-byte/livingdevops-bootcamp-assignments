35128440248bcc05676de1f3904e97be50c4dd3d
ashishkr-tech <ashishjp82@gmail.com>
2026-06-21 17:48:16 +0530

    adding the modified files

--- week6/infra/versions.tf
+++ week6/infra/versions.tf
@@ -1,16 +1,16 @@
 
 terraform {
-  required_version = "1.14.1"
+  required_version = "1.14.1" # this is the terraform cli version
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.0" # this is aWS provider plugin version, i.e. use aws provider version >5
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~> 3.0" # this is random provider plugin version, i.e. use random provider version >3
     }
   }
 }
